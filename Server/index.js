7a4e7264ac5aa9f4f9457b77b53792e194c17d22
frederic Botella <frederic.botella@gmail.com>
2023-03-27 17:22:49 +0200

    create files: index.js, .env, .gitignore

---
+++ Server/index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 3000;
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/', (req,res)=> {
+    res.json('welcome !');
+});
+
+app.listen(port, () => {
+    console.log(`Server started on http://localhost:${port}`);
+});
